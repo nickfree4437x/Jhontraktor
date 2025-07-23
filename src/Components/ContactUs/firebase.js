bdd195ac96036fcbd7a187e8bc7cefd506a9311e
vishal-saini750 <vishalsaini5678niwarkhas@gmail.com>
2025-07-23 17:56:02 +0530

    Added form validation and Firestore error handling

--- src/Components/ContactUs/firebase.js
+++ src/Components/ContactUs/firebase.js
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCzm7fiHxcEUqGsDd2FzMlthArOeJUM-lM",
   authDomain: "jhontraktor.firebaseapp.com",
+  databaseURL: "https://jhontraktor-default-rtdb.firebaseio.com",
   projectId: "jhontraktor",
   storageBucket: "jhontraktor.firebasestorage.app",
   messagingSenderId: "667121759203",
-  appId: "1:667121759203:web:85569e8cec6571348fe6d6"
+  appId: "1:667121759203:web:85569e8cec6571348fe6d6",
+  measurementId: "G-X92FB4V9W3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
